51a182df8b22b830db376869c18323f83cc1585e
Omk4314 <vijaykolhapure0236@gmail.com>
2026-05-05 01:04:51 +0530

    Day 1

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-word = "Library"
-print(word.split("r", -1))
+#My first python code
+print("Hello, World")
+#Making a greeter!!
+#Ask user for his/her name
+name = input("What's your name? ")
+#methods to display the greeting on the screen as hello, name(whatever the name might be)
+#explicitly mentioning value of end in print function
+print("Hello, ", end = " ")
+print(name)
+#Giving two arguments to the print function
+print("Hello,", name)
+#Concatening Strings
+print("Hello, " + name)
+#Using f-strings
+print(f"Hello, {name}") #The most commonly used one 
