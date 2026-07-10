0970fe9b2ecd40035de3e199abd9dedf455acd96
Omk4314 <kolhapureom4314@gmail.com>
2026-07-10 14:46:02 +0530

    Day 67: Added jwt token authentication

--- auth_book_api.py
+++ auth_book_api.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime, timedelta
 
-SECRET_KEY = "my-key"
+SECRET_KEY = "G7pxwL9"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
