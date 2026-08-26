0e906dd8f251778c5bea65301eadf303ab6652f0
Omk4314 <kolhapureom4314@gmail.com>
2026-08-26 20:47:21 +0530

    Day 80: Built a simple chatbot using openai api

---
+++ ChatBot/bot.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    response = client.responses.create(
+        model = "gpt-5.6",
+        input = user_input
+    )
+    print("Bot: ", response.output_text)
